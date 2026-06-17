a6b95f7ce16d8df52894a15600d578df2c71e611
AmanditasGithu <amivillaben1432@gmail.com>
2026-06-17 14:48:26 -0600

    Add files via upload

---
+++ CalculadoraTrig.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;  //Importo la herramienta para leer la terminal. Fun fact: a diferencia de Python en Java los comentarios son con //
+public class CalculadoraTrig { //Java trabaja dentro de clases absolutamente todo codigo, la clase debe de llamarse igual que el archivo
+    public static void main (String[]args){ //Por default todo programa en Java debe empezar de esta forma, todo lo que hay dentro de los corchetes se ejecuta en ese orden
+        Scanner lector = new Scanner(System.in); //Scanner lector funciona para leer y capturar datos de entrada. En este caso es para leer lo de la terminal
+        System.out.println("CALCULADORA DE FUNCIONES HIPERBOLICAS");//println hace un print por linea, print es mas simple. Por default terminan con ;
+        System.out.print("Introduzca el valor de un angulo");
+        double angulo = lector.nextDouble(); //Se lee el numero que escribe el usuario y se guarda en la variable angulo. Double se usa en Java para indicar decimales 
+        double senoHiperbolico = Math.sinh(angulo); //Para calcular las funciones hiperbolicas se utilizan las librerias predeterminadas de MATH 
+        double cosenoHiperbolico = Math.cosh(angulo);//Ellas hacen el calculo de una y lo entregan
+        double tangenteHiperbolico = Math.tanh(angulo);
+
+        System.out.println("Resultados del angulo ="+angulo+":"); //+ sirve para concatenar en este caso
+        System.out.println("Seno =" + senoHiperbolico);
+        System.out.println("Coseno =" + cosenoHiperbolico);
+        System.out.println("Tangente = " + tangenteHiperbolico);
+        lector.close(); //cierro el lector 
+
+
+    }
+}
+
